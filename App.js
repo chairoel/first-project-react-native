c9f6ced4e911a8bc7fccb8020a6884eed4059d9c
chairul <chairul.amri@ebconnect.com>
2024-09-28 17:53:49 +0700

    add image from url link

--- App.js
+++ App.js
@@ -29,6 +29,15 @@ const App = () => {
           style={{width: 250, height: 250}}
         />
       </View>
+
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1727461567487-575ec98777fc?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          }}
+          style={{width: 250, height: 250}}
+        />
+      </View>
     </View>
   );
 };
