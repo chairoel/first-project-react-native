7c02902d1e7283570f7897222eca628eb1f3e413
chairul <chairul.amri@ebconnect.com>
2024-09-28 19:09:42 +0700

    touchable opacity styling like button>

--- App.js
+++ App.js
@@ -10,8 +10,20 @@ const App = () => {
     <View style={{flex: 1, backgroundColor: '#282c34'}}>
       <StatusBar backgroundColor={'#282c34'} barStyle={'light-content'} />
 
-      <TouchableOpacity onPress={() => hello('cinta')}>
-        <Text style={{fontSize: 48, color: 'white'}}>Hello</Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#0288d1',
+          marginHorizontal: 30,
+          marginTop: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+
+          paddingTop: 10,
+          paddingBottom: 10,
+          borderRadius: 8,
+        }}
+        onPress={() => hello('cinta')}>
+        <Text style={{fontSize: 18, color: 'white'}}>Hello</Text>
       </TouchableOpacity>
     </View>
   );
