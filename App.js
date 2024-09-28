68c590ef010ac6a7ea74399edd3c62b1f49d3f23
chairul <chairul.amri@ebconnect.com>
2024-09-28 16:04:40 +0700

    styling status bar

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StatusBar} from 'react-native';
 
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: '#21252b',
-        paddingHorizontal: 20,
-        paddingVertical: 20,
-        elevation: 3,
-      }}>
-      <Text
+    <View>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#21252b'} />
+      <View
         style={{
-          color: '#FFFFFF',
-          fontWeight: 'bold',
-          fontSize: 18,
-          textAlign: 'center',
+          backgroundColor: '#21252b',
+          paddingHorizontal: 20,
+          paddingVertical: 20,
+          elevation: 3,
         }}>
-        Aplikasoi kaomoji
-      </Text>
+        <Text
+          style={{
+            color: '#FFFFFF',
+            fontWeight: 'bold',
+            fontSize: 18,
+            textAlign: 'center',
+          }}>
+          Aplikasoi kaomoji
+        </Text>
+      </View>
     </View>
   );
 };
