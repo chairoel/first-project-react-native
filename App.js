3aa108f6b830a3b8e72571e09b26f31d544c67bc
chairul <chairul.amri@ebconnect.com>
2024-09-28 22:15:29 +0700

    initial switch

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {Alert, Text, TouchableOpacity, View} from 'react-native';
-
-let saldo = 100000;
+import {View} from 'react-native';
 
 const App = () => {
-  const [saldoSaya, setSaldoSaya] = useState(100000);
-
-  function hitungDiskon() {
-    let perhitungan;
-    perhitungan = saldoSaya - 10000;
-    setSaldoSaya(perhitungan);
-
-    // saldo = saldo - 10000;
-    // Alert.alert('Saldo saya: ' + saldo);
-  }
-
   return (
     <View>
-      <Text>{saldoSaya}</Text>
-      <TouchableOpacity onPress={() => hitungDiskon()}>
-        <Text>Hitung Diskon</Text>
-      </TouchableOpacity>
+      <Text></Text>
     </View>
   );
 };
