54d73bdc974cedc173a0db7ddbab84db9d8a5cbe
chairul <chairul.amri@ebconnect.com>
2024-09-28 15:04:32 +0700

    set style text

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import {Text, View} from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 48,
+          fontWeight: '900',
+          color: '#3627ab',
+        }}>
+        Hello World
+      </Text>
+      <Text>Description 1</Text>
     </View>
   );
 };
