632122878b16c4b2717c437e76e9212a3ce75c8e
chairul <chairul.amri@ebconnect.com>
2024-09-28 18:55:42 +0700

    setup touchable opacity

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {StatusBar, View} from 'react-native';
+import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1, backgroundColor: '#282c34'}}>
       <StatusBar backgroundColor={'#282c34'} barStyle={'light-content'} />
+
+      <TouchableOpacity>
+        <Text style={{fontSize: 48, color: 'white'}}>Hello</Text>
+      </TouchableOpacity>
     </View>
   );
 };
