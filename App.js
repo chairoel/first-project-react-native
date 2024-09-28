b9abe592ffed3b958c24aa7a68149c90618d2134
chairul <chairul.amri@ebconnect.com>
2024-09-28 22:35:47 +0700

    create login page (add email using text input)

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import React, {useState, useEffect} from 'react';
-import {View} from 'react-native';
+import {StatusBar, Text, TextInput, View} from 'react-native';
 
 const App = () => {
-  return <View></View>;
+  return (
+    <View>
+      <StatusBar backgroundColor={'#fafafa'} barStyle={'dark-content'} />
+
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 18,
+          fontWeight: 'bold',
+          marginTop: 24,
+        }}>
+        Login
+      </Text>
+      <Text style={{textAlign: 'center'}}>
+        Silahkan masukkan email dan password
+      </Text>
+
+      <TextInput
+        style={{
+          backgroundColor: '#FFFFFF',
+          elevation: 2,
+          marginHorizontal: 20,
+          marginTop: 20,
+          borderRadius: 8,
+          paddingLeft: 10,
+          paddingRight: 10,
+        }}
+        placeholder="Masukkan Email"
+      />
+    </View>
+  );
 };
 
 export default App;
