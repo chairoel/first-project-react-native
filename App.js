1b5fec0700b66d280d666ddf5208beff5cbe7a13
chairul <chairul.amri@ebconnect.com>
2024-09-28 15:43:00 +0700

    style lanjutan

--- App.js
+++ App.js
@@ -6,14 +6,46 @@ const App = () => {
     <View>
       <Text
         style={{
-          textAlign: 'center',
-          fontSize: 48,
-          fontWeight: '900',
-          color: '#3627ab',
+          textAlign: 'auto',
+          fontSize: 16,
+          // fontWeight: '900',
+          // color: '#3627ab',
+          // marginTop: 30,
+          // marginBottom: 30,
+          // marginVertical: 30,
+          // marginLeft: 20,
+          // marginRight: 20,
+          // marginHorizontal: 30,
+          // margin: 10,
+
+          // letterSpacing: 20,
+          // lineHeight: 25,
         }}>
-        Hello World
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum
+        arcu et enim sodales vehicula. Fusce imperdiet sem nec orci malesuada,
+        ac euismod ipsum pharetra. Aliquam vel tellus a mauris iaculis blandit.
+        Nunc nisi sem, fermentum at metus eu, eleifend vestibulum tellus. Nullam
+        vestibulum magna vitae hendrerit sagittis. In at imperdiet est, ut
+        iaculis tellus. Nam sodales aliquet justo in aliquet.
       </Text>
       <Text>Description 1</Text>
+
+      <View
+        style={{
+          marginVertical: 12,
+          width: 200,
+          height: 200,
+          backgroundColor: 'red',
+          borderRadius: 9,
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          flexDirection: 'column',
+        }}>
+        <Text style={{color: 'white'}}>Hello</Text>
+        <Text style={{color: 'white'}}>Hello</Text>
+        <Text style={{color: 'white'}}>Hello</Text>
+        <Text style={{color: 'white'}}>Hello</Text>
+      </View>
     </View>
   );
 };
