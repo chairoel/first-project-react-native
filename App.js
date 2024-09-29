e1a7130450e08c7af028677abd3006f7ca90ac54
chairul <chairul.amri@ebconnect.com>
2024-09-29 15:41:38 +0700

    linking react native

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
 import React, {useState, useEffect} from 'react';
-import {ImageBackground, Text, View} from 'react-native';
+import {
+  ImageBackground,
+  Linking,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      <ImageBackground
-        source={require('./src/images/pemandangan.jpg')}
-        style={{marginHorizontal: 30, marginTop: 10}}
-        imageStyle={{borderRadius: 8}}>
-        <View
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            padding: 15,
-            borderRadius: 8,
-          }}>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-          <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
-        </View>
-      </ImageBackground>
+      <TouchableOpacity
+        onPress={() => Linking.openURL('https://www.google.com/')}>
+        <ImageBackground
+          source={require('./src/images/pemandangan.jpg')}
+          style={{marginHorizontal: 30, marginTop: 10}}
+          imageStyle={{borderRadius: 8}}>
+          <View
+            style={{
+              backgroundColor: 'rgba(0,0,0,0.5)',
+              padding: 15,
+              borderRadius: 8,
+            }}>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+            <Text style={{color: '#FFFFFF'}}>Image Bacground</Text>
+          </View>
+        </ImageBackground>
+      </TouchableOpacity>
     </View>
   );
 };
