cf63cf4a85734c851256a3af9b5c0f45d6dc44f3
chairul <chairul.amri@ebconnect.com>
2024-09-29 11:22:03 +0700

    show list data using flat list

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import React, {useState, useEffect} from 'react';
-import {
-  StatusBar,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Button,
-  Alert,
-} from 'react-native';
+import {FlatList, StatusBar, Text, View} from 'react-native';
+
+const negara = [
+  'indonesia',
+  'jepang',
+  'amerika',
+  'afrika',
+  'chaina',
+  'thailand',
+  'australia',
+];
 
 const App = () => {
   return (
-    <View>
-      <View style={{marginHorizontal: 24, marginTop: 12}}>
-        <Button
-          title="Login"
-          color={'#212121'}
-          onPress={() => Alert.alert('hello world')}
-        />
-      </View>
+    <View style={{flex: 1, backgroundColor: '#21252b'}}>
+      <StatusBar backgroundColor={'#21252b'} barStyle={'light-content'} />
+
+      <FlatList
+        data={negara}
+        renderItem={({item, index}) => (
+          <View
+            style={{
+              marginHorizontal: 20,
+              marginTop: 10,
+              marginBottom: 10,
+              backgroundColor: '#282c34',
+              paddingTop: 50,
+              paddingBottom: 50,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 4,
+              elevation: 3,
+            }}>
+            <Text style={{color: '#FFFFFF'}}>{item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
